b4bde24b949ec970ff2a5bbf4bc4eba6eb9db5d2
ahmed1700 <ahmed@ahmed.com>
2019-10-14 12:17:46 -0700

    All

---
+++ WebAPI/Controllers/MyTasksController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyTasksController : ControllerBase
+    {
+        private readonly TaskContext _context;
+
+        public MyTasksController(TaskContext context)
+        {
+            _context = context;
+            if (_context.MyTasks.Count()==0)
+            {
+                _context.MyTasks.Add(new MyTask { Name="My First Task"});
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/MyTasks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MyTask>>> Gettasks()
+        {
+            return await _context.MyTasks.ToListAsync();
+        }
+
+        // GET: api/MyTasks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MyTask>> GetMyTask(long id)
+        {
+            var myTask = await _context.MyTasks.FindAsync(id);
+
+            if (myTask == null)
+            {
+                return NotFound();
+            }
+
+            return myTask;
+        }
+
+        // PUT: api/MyTasks/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMyTask(long id, MyTask myTask)
+        {
+            if (id != myTask.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(myTask).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MyTaskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/MyTasks
+        [HttpPost]
+        public async Task<ActionResult<MyTask>> PostMyTask(MyTask myTask)
+        {
+            _context.MyTasks.Add(myTask);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMyTask", new { id = myTask.Id }, myTask);
+        }
+
+        // DELETE: api/MyTasks/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<MyTask>> DeleteMyTask(long id)
+        {
+            var myTask = await _context.MyTasks.FindAsync(id);
+            if (myTask == null)
+            {
+                return NotFound();
+            }
+
+            _context.MyTasks.Remove(myTask);
+            await _context.SaveChangesAsync();
+
+            return myTask;
+        }
+
+        private bool MyTaskExists(long id)
+        {
+            return _context.MyTasks.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebAPI/Data/TaskContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+
+        }
+        public DbSet<MyTask> MyTasks { get; set; }
+    }
+}
